811bb0d2d06a041ea351f70c8ce4d499d7dd9a42
lerrary <zparol911@gmail.com>
2024-10-24 10:24:48 +0500

    Update module_9_1.py

--- module_9_1.py
+++ module_9_1.py
@@ -8,21 +8,5 @@ def apply_all_func(int_list, *functions):
         results.update({i.__name__: i(int_list)})
     return results
 
-def __min__(int_list):
-    return min(int_list)
-
-def __max__(int_list):
-    return max(int_list)
-
-def __len__(int_list):
-    return len(int_list)
-
-def __sum__(int_list):
-    return sum(int_list)
-
-def __sorted__(int_list):
-    return sorted(int_list)
-
-
 print(apply_all_func([6, 20, 15, 9], max, min))
 print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
