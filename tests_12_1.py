8bc6b2040378184f8a78da4a542d12bfa0470274
lerrary <zparol911@gmail.com>
2024-11-21 22:47:49 +0500

    Update tests_12_1.py

--- tests_12_1.py
+++ tests_12_1.py
@@ -2,19 +2,23 @@ import task
 import unittest
 
 class RunnerTest(unittest.TestCase):
+    iz_frozen = False
 
+    @unittest.skipIf(iz_frozen, 'Тесты в этом кейсе заморожены')
     def test_walk(self):
         self.walk1 = task.Runner('1')
         for i in range(10):
             self.walk1.walk()
         self.assertEqual(self.walk1.distance, 50)
 
+    @unittest.skipIf(iz_frozen, 'Тесты в этом кейсе заморожены')
     def test_run(self):
         self.run2 = task.Runner('2')
         for i in range(10):
             self.run2.run()
         self.assertEqual(self.run2.distance, 100)
 
+    @unittest.skipIf(iz_frozen, 'Тесты в этом кейсе заморожены')
     def test_challenge(self):
         self.chal3 = task.Runner('3')
         self.chal4 = task.Runner('4')
