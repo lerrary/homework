d1ddec751323adea56dd95080f5d4bdcc0f2cb18
lerrary <zparol911@gmail.com>
2025-01-12 11:29:22 +0500

    Update module_16_3.py

--- module_16_3.py
+++ module_16_3.py
@@ -12,8 +12,6 @@ async def get_users() -> dict:
 async def add_user(username: str, age: int) -> str:
     new_id = str(int(max(users, key = int))+ 1)
     users[new_id] = f'Имя: {username}, возраст: {age}'
-    #new_user = {"id": new_id, "description": description}
-    #users.append(new_user)
     return f"User {new_id} is registered"
 
 @app.put("/user/{user_id}/{username}/{age}")
